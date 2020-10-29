a39c443d56c2e9f02d65bdde93d470e96e75106e
Punchcode <punchcode@Punchcodes-MacBook-Pro.local>
2020-10-29 14:16:43 -0700

    started database, auth route and users route

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3001
 const usersRoute = require('./routes/users')
 app.use(express.json())
-app.use(express.urlencoded({ extended: false}))
+app.use(express.urlencoded({ extended: false }))
 app.use('/api', usersRoute)
 app.listen(PORT, () => {
   console.log("listening on port" + PORT)
--- server/db-creation.js
+++ server/db-creation.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
 const conn = require('./db')
-// const sha512 = require('js-sha512')
-// const { createSalt } = require('./utils/auth')
+const sha512 = require('js-sha512')
+const { createSalt } = require('./utils/auth')
 
 const tables = [
   'users',
   'goals',
-  'posts',
-  'tasks'
+  'tasks',
+  'posts'
 ]
 
 async function main() {
@@ -28,7 +28,27 @@ async function main() {
     table.increments('id')
     table.string('title', 30)
     table.string('reason', 30)
+    table.datetime('finish_line')
+    table.integer('user_id').unsigned()
+    table.foreign('user_id').references('users.id')
   }) 
+
+  await conn.schema.createTable(`tasks`, (table) => {
+    table.increments('id')
+    table.string('description', 50)
+    table.increments('parent_id')
+    table.integer('goal_id').unsigned()
+    table.foreign('goal_id').references('goals_id')
+  })
+
+  await conn.schema.createTable(`posts`, (table) => {
+    table.increments('id')
+    table.datetime('date_time')
+    table.string('description', 144)
+    table.integer('goal_id').unsigned()
+    table.foreign('goal_id').references('goals_id')
+  })
+
   process.exit()
 }
 
