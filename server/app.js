32795794ad83b09d1c62b92b351dddf5d827f588
Punchcode <punchcode@Punchcodes-MacBook-Pro.local>
2020-10-29 15:24:49 -0700

    all routes complete

--- server/app.js
+++ server/app.js
@@ -2,10 +2,20 @@ require('dotenv').config()
 const express = require('express')
 const app = express();
 const PORT = 3001
+
 const usersRoute = require('./routes/users')
+const goalsRoute = require('./routes/goals')
+const tasksRoute = require('./routes/tasks')
+const postsRoute = require('./routes/posts')
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
 app.use('/api', usersRoute)
+app.use('/api', goalsRoute)
+app.use('/api', tasksRoute)
+app.use('/api', postsRoute)
+
 app.listen(PORT, () => {
   console.log("listening on port" + PORT)
 })
