4e5e32f2c178d2996e5bb50d829dc0cad8c41a9a
Jesse Harvey <jesselharvey@gmail.com>
2020-10-30 17:02:01 -0700

    add test data

--- client/src/features/components/goals/goalsSlice.js
+++ client/src/features/components/goals/goalsSlice.js
@@ -1,10 +1,37 @@
 import { createSlice } from '@reduxjs/toolkit'
+import axios from 'axios'
 
 export const goalsSlice = createSlice({
   name: 'goals',
   initialState: {
-    userInfo: {},
-    goals: [],
+    userInfo: {
+      id: 1,
+      name: "Jesse",
+      email: "ex@example.com"
+    },
+    goals: [
+      {
+        id: 1,
+        title: "stop smoking",
+        reason: "it's unhealthy"
+      },      {
+        id: 2,
+        title: "practice cello",
+        reason: "it's soothing"
+      },      {
+        id: 3,
+        title: "read more",
+        reason: "it expands my mind"
+      },      {
+        id: 4,
+        title: "exercise",
+        reason: "it's healthy"
+      },      {
+        id: 5,
+        title: "stop drinking",
+        reason: "it's unhealthy too"
+      },
+    ],
   },
   reducers: {
     asyncFetchGoals: (state, action) => {
@@ -15,15 +42,25 @@ export const goalsSlice = createSlice({
     },
     removeGoalFunct: (state, action) => {
       state.goals = state.goals.filter((goal) => {
-        goal.id !== action.payload.id
+      return goal.id !== action.payload.id
       })
     },
     editGoalFunct: (state, action) => {
       state.goals = state.goals.filter((goal) => {
-      goal.id !== action.payload.id
+      return goal.id !== action.payload.id
       })
     },
   },
 })
 
-export const { asyncFetchGoals, addGoalFunct, removeGoalFunct, editGoalFunct} = 
+export const { asyncFetchGoals, addGoalFunct, removeGoalFunct, editGoalFunct} = goalsSlice.actions
+
+export const fetchGoals = () => {
+  axios.get('/api/goals').then((resp) => {
+    dispatch(asyncFetchGoals(resp.data))
+  })
+}
+
+// export const addGoal = (goal) => {
+//   axios.post
+// }
